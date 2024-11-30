bcf3529cd4781771d454c6ce6b931e7eca4799ad
michelle schneider <michesc@edu.aac.ac.il>
2024-11-30 20:26:29 +0200

    commit

--- src/Components/GamePage.js
+++ src/Components/GamePage.js
@@ -1,10 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import './GamePage.css'
 
 function GamePage({ userName, difficulty }) {
+    const [seconds, setSeconds] = useState(0);
+    const [isGameEnded, setIsGameEnded] = useState(false);
+    const [timer, setTimer] = useState(null);
+
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes < 10 ? '0' : ''}${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    };
+
+    useEffect(() => {
+        if (!isGameEnded) {
+            const newTimer = setInterval(() => {
+                setSeconds(prevSeconds => prevSeconds + 1);
+            }, 1000);
+            setTimer(newTimer);
+
+            return () => clearInterval(newTimer);
+        }
+    }, [isGameEnded]);
+
+
+    const endGame = () => {
+        setIsGameEnded(true);
+        clearInterval(timer);
+    };
+
     return (
-        <div>
-            <h1>Hello, {userName}!</h1>
-            <p>You selected difficulty level: {difficulty} cards</p>
+        <div className="game-page">
+            <div className="game-info">
+                <div className="intro">
+                    <h4>hey, {userName}!</h4>
+                    <p>You selected difficulty level: {difficulty} cards</p>
+                </div>
+                <div className="timer">
+                    {formatTime(seconds)}
+                </div>
+            </div>
+
+
+            // The game
+
+
+            <button onClick={endGame} className="end-button">End Game</button>
         </div>
-    )
+    );
 }
+
 export default GamePage;
