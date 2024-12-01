f37229f7ebd1dbbeb7ba3b93aaef5ed2733fb249
michelle schneider <michesc@edu.aac.ac.il>
2024-12-01 22:38:41 +0200

    commit

--- src/Components/GamePage.js
+++ src/Components/GamePage.js
@@ -19,6 +19,8 @@ function GamePage({ userName, difficulty }) {
             }, 1000);
             setTimer(newTimer);
 
+            shuffleImage();
+
             return () => clearInterval(newTimer);
         }
     }, [isGameEnded]);
@@ -29,6 +31,16 @@ function GamePage({ userName, difficulty }) {
         clearInterval(timer);
     };
 
+    const shuffleImage = () => {
+        const cards = document.querySelectorAll('.cell');  // בוחר את כל הקלפים
+        const num = [...Array(cards.length).keys()];  // יוצר מערך של אינדקסים מ-0 עד לאורך המערך
+        num.sort(() => Math.random() - 0.5);  // מערבב את האינדקסים באופן רנדומלי
+
+        cards.forEach((card, index) => {
+            card.style.order = num[index];  // משנה את הסדר של כל קלף
+        });
+    };
+
     return (
         <div className="game-page">
             <div className="game-info">
@@ -36,23 +48,164 @@ function GamePage({ userName, difficulty }) {
                     <h4>hey, {userName}!</h4>
                     <p>You selected difficulty level: {difficulty} cards</p>
                 </div>
-                <div className="timer">
-                    {formatTime(seconds)}
-                </div>
             </div>
 
-
-            // The game
-            <div className="container" >
+            <div className="container">
                 <div className="cell">
-                    <div className="front" data-index="1"> </div>
+                    <div className="front" data-index="1"><img src={require('../Styles/Images/sticker3.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
                 </div>
-
-
-
-
+                <div className="cell">
+                    <div className="front" data-index="1"><img src={require('../Styles/Images/sticker3.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="2"><img src={require('../Styles/Images/sticker4.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="2"><img src={require('../Styles/Images/sticker4.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="3"><img src={require('../Styles/Images/sticker5.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="3"><img src={require('../Styles/Images/sticker5.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="4"><img src={require('../Styles/Images/sticker6.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="4"><img src={require('../Styles/Images/sticker6.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="5"><img src={require('../Styles/Images/sticker7.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="5"><img src={require('../Styles/Images/sticker7.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="6"><img src={require('../Styles/Images/sticker8.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="6"><img src={require('../Styles/Images/sticker8.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="7"><img src={require('../Styles/Images/sticker9.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="7"><img src={require('../Styles/Images/sticker9.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="8"><img src={require('../Styles/Images/sticker10.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="8"><img src={require('../Styles/Images/sticker10.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="9"><img src={require('../Styles/Images/sticker11.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="9"><img src={require('../Styles/Images/sticker11.webp')} alt=""/>
+                    </div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="10"><img src={require('../Styles/Images/sticker12.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="10"><img src={require('../Styles/Images/sticker12.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="11"><img src={require('../Styles/Images/sticker13.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="11"><img src={require('../Styles/Images/sticker13.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="12"><img src={require('../Styles/Images/sticker14.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="12"><img src={require('../Styles/Images/sticker14.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="13"><img src={require('../Styles/Images/sticker15.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="13"><img src={require('../Styles/Images/sticker15.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="14"><img src={require('../Styles/Images/sticker16.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="14"><img src={require('../Styles/Images/sticker16.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="15"><img src={require('../Styles/Images/sticker17.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
+                <div className="cell">
+                    <div className="front" data-index="15"><img src={require('../Styles/Images/sticker17.webp')}
+                                                                alt=""/></div>
+                    <div className="back"></div>
+                </div>
             </div>
 
+            <div className="timer">
+                {formatTime(seconds)}
+            </div>
 
             <button onClick={endGame} className="end-button">End Game</button>
         </div>
